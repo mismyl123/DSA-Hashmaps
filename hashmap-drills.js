22df601e723d8d3a1ff6302c431b6a70cca0196a
mismyl123 <mlalonde309@gmail.com>
2020-12-23 19:41:54 -0500

    hashmap2

--- hashmap-drills.js
+++ hashmap-drills.js
@@ -1,8 +1,15 @@
-const HashMap = require("./hashmap");
+const HashMap = require("./hashmaps");
 
 function main() {
   const lotr = new HashMap();
 
   lotr.MAX_LOAD_RATIO = 0.5;
   lotr.SIZE_RATIO = 3;
+
+  lotr.assign("Hobbit", "Bilbo");
+  lotr.assign("Hobbit", "Frodo");
+
+  console.log(lotr);
 }
+
+main();
